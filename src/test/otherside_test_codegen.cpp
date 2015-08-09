1194f2dfca42efff7a557defe025e8b15900418b
Dario Seyb <dario.seyb@gmail.com>
2015-08-09 03:59:48 +0100

    added more tests

---
+++ src/test/otherside_test_codegen.cpp
@@ -0,0 +1,18 @@
+#include "parser_definitions.h"
+#include "parser.h"
+#include "codegen.h"
+
+int main(int argc, char** argv) {
+    Parser parser(argv[1]);
+
+    Program prog;
+    if(!parser.ParseProgram(&prog)) {
+        return -1;
+    }
+    
+    if (!genCode(argv[2], prog)) {
+        return -1;
+    }
+
+    return 0;
+}
